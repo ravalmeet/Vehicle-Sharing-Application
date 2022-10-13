c991d517d98d5b5399b158b8674bc4b4b32f21a5
MadhuSaini22 <madhusaini46810@gmail.com>
2022-10-13 13:13:13 +0530

    initial commit

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
     secret_jwt: "khuljasimsim",
     emailUser: "madhucs987@gmail.com", 
-    emailPassword:"ehiyavgzdcjurstq",
+    emailPassword:"0ehi08yavg6z2dcjurstq1",
   };
   
